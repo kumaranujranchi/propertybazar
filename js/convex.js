4cc13bf992e8fb68740d10384022adf002501614
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2026-02-20 11:33:43 +0530

    chore: configuration for netlify deployment

--- js/convex.js
+++ js/convex.js
@@ -1,3 +1,3 @@
-import { ConvexHttpClient } from "https://esm.sh/convex@1.16.0/browser";
+import { ConvexHttpClient } from "https://esm.sh/convex@1.32.0/browser";
 
 export const convex = new ConvexHttpClient("https://veracious-caribou-870.convex.cloud");
